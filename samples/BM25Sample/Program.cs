45fa2b240fbb96f0fa4e640c33c2ae2241e725fc
Philippe Laval <philippe.laval@qualiparc.net>
2026-07-27 12:52:57 +0200

    test

--- samples/BM25Sample/Program.cs
+++ samples/BM25Sample/Program.cs
@@ -9,7 +9,7 @@ namespace BM25Sample
         {
             var docs = new List<string>
                 {
-                    "The quick brown fox jumps over the lazy dog.",
+                    "The quick brown fox jumps over the lazy dog. Phi",
                     "The fox",
                     "The dog",
                     "A brown dog jumps over another dog.",
